2cb44d5d654d7724fd14ea6f0ce468478003c518
Michael Ruocco <michael.ruocco@hotmail.com>
2021-05-03 07:09:47 +0100

    adds tests for validating values against bounds of time units

--- domain/timeUnit.go
+++ domain/timeUnit.go
@@ -1,28 +1,33 @@
 package domain
 
+import (
+	"fmt"
+)
+
 type timeUnit struct {
+	name       string
 	lowerBound int
 	upperBound int
 }
 
 func Minute() timeUnit {
-	return timeUnit{lowerBound: 0, upperBound: 59}
+	return timeUnit{"minute", 0, 59}
 }
 
 func Hour() timeUnit {
-	return timeUnit{lowerBound: 0, upperBound: 23}
+	return timeUnit{"hour", 0, 23}
 }
 
 func DayOfMonth() timeUnit {
-	return timeUnit{lowerBound: 1, upperBound: 31}
+	return timeUnit{"day of month", 1, 31}
 }
 
 func Month() timeUnit {
-	return timeUnit{lowerBound: 1, upperBound: 12}
+	return timeUnit{"month", 1, 12}
 }
 
 func DayOfWeek() timeUnit {
-	return timeUnit{lowerBound: 0, upperBound: 6}
+	return timeUnit{"day of week", 0, 6}
 }
 
 func AllValues(timeUnit timeUnit) []int {
@@ -32,4 +37,29 @@ func AllValues(timeUnit timeUnit) []int {
 		values[i] = min + i
 	}
 	return values
+}
+
+func ValidateMultiple(timeUnit timeUnit, inputs []int) error {
+	for _, input := range inputs {
+		err := Validate(timeUnit, input)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Validate(timeUnit timeUnit, input int) error {
+	if timeUnit.isOutOfBounds(input) {
+		return fmt.Errorf("invalid %v value %d outside bounds %d and %d",
+			timeUnit.name,
+			input,
+			timeUnit.lowerBound,
+			timeUnit.upperBound)
+	}
+	return nil
+}
+
+func (timeUnit timeUnit) isOutOfBounds(input int) bool {
+	return input < timeUnit.lowerBound || input > timeUnit.upperBound
 }
--- domain/timeUnit_test.go
+++ domain/timeUnit_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShouldReturnAllMinuteValues(t *testing.T) {
+func TestMinuteShouldReturnAllValues(t *testing.T) {
 	timeUnit := Minute()
 
 	allValues := AllValues(timeUnit)
@@ -14,7 +14,34 @@ func TestShouldReturnAllMinuteValues(t *testing.T) {
 	assert.Equal(t, allValues, allRangeValuesIncluding(0, 59))
 }
 
-func TestShouldReturnAllHourValues(t *testing.T) {
+func TestMinuteShouldNotReturnErrorIfValuesAreWithinBounds(t *testing.T) {
+	timeUnit := Minute()
+	values := []int{0, 59}
+
+	err := ValidateMultiple(timeUnit, values)
+
+	assert.Equal(t, err, nil)
+}
+
+func TestMinuteShouldReturnErrorIfValueIsLessThanLowerBound(t *testing.T) {
+	timeUnit := Minute()
+	value := -1
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid minute value -1 outside bounds 0 and 59")
+}
+
+func TestMinuteShouldReturnErrorIfValueIsGreaterThanUpperBound(t *testing.T) {
+	timeUnit := Minute()
+	value := 60
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid minute value 60 outside bounds 0 and 59")
+}
+
+func TestHourShouldReturnAllValues(t *testing.T) {
 	timeUnit := Hour()
 
 	allValues := AllValues(timeUnit)
@@ -22,7 +49,34 @@ func TestShouldReturnAllHourValues(t *testing.T) {
 	assert.Equal(t, allValues, allRangeValuesIncluding(0, 23))
 }
 
-func TestShouldReturnAllDayOfMonthValues(t *testing.T) {
+func TestHourShouldNotReturnErrorIfValuesAreWithinBounds(t *testing.T) {
+	timeUnit := Hour()
+	values := []int{0, 23}
+
+	err := ValidateMultiple(timeUnit, values)
+
+	assert.Equal(t, err, nil)
+}
+
+func TestHourShouldReturnErrorIfValueIsLessThanLowerBound(t *testing.T) {
+	timeUnit := Hour()
+	value := -1
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid hour value -1 outside bounds 0 and 23")
+}
+
+func TestHourShouldReturnErrorIfValueIsGreaterThanUpperBound(t *testing.T) {
+	timeUnit := Hour()
+	value := 24
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid hour value 24 outside bounds 0 and 23")
+}
+
+func TestDayOfMonthShouldReturnAllValues(t *testing.T) {
 	timeUnit := DayOfMonth()
 
 	allValues := AllValues(timeUnit)
@@ -30,7 +84,34 @@ func TestShouldReturnAllDayOfMonthValues(t *testing.T) {
 	assert.Equal(t, allValues, allRangeValuesIncluding(1, 31))
 }
 
-func TestShouldReturnAllMonthValues(t *testing.T) {
+func TestDayOfMonthShouldNotReturnErrorIfValuesAreWithinBounds(t *testing.T) {
+	timeUnit := DayOfMonth()
+	values := []int{1, 31}
+
+	err := ValidateMultiple(timeUnit, values)
+
+	assert.Equal(t, err, nil)
+}
+
+func TestDayOfMonthShouldReturnErrorIfValueIsLessThanLowerBound(t *testing.T) {
+	timeUnit := DayOfMonth()
+	value := 0
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid day of month value 0 outside bounds 1 and 31")
+}
+
+func TestDayOfMonthShouldReturnErrorIfValueIsGreaterThanUpperBound(t *testing.T) {
+	timeUnit := DayOfMonth()
+	value := 32
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid day of month value 32 outside bounds 1 and 31")
+}
+
+func TestMonthShouldReturnAllValues(t *testing.T) {
 	timeUnit := Month()
 
 	allValues := AllValues(timeUnit)
@@ -38,12 +119,66 @@ func TestShouldReturnAllMonthValues(t *testing.T) {
 	assert.Equal(t, allValues, allRangeValuesIncluding(1, 12))
 }
 
-func TestShouldReturnAllDayOfWeekValues(t *testing.T) {
+func TestMonthShouldNotReturnErrorIfValuesAreWithinBounds(t *testing.T) {
+	timeUnit := Month()
+	values := []int{1, 12}
+
+	err := ValidateMultiple(timeUnit, values)
+
+	assert.Equal(t, err, nil)
+}
+
+func TestMonthShouldReturnErrorIfValueIsLessThanLowerBound(t *testing.T) {
+	timeUnit := Month()
+	value := 0
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid month value 0 outside bounds 1 and 12")
+}
+
+func TestMonthShouldReturnErrorIfValueIsGreaterThanUpperBound(t *testing.T) {
+	timeUnit := Month()
+	value := 13
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid month value 13 outside bounds 1 and 12")
+}
+
+func TestDayOfWeekShouldReturnValues(t *testing.T) {
 	timeUnit := DayOfWeek()
 
 	allValues := AllValues(timeUnit)
 
 	assert.Equal(t, allValues, allRangeValuesIncluding(0, 6))
+}
+
+func TestDayOfWeekShouldNotReturnErrorIfValuesAreWithinBounds(t *testing.T) {
+	timeUnit := DayOfWeek()
+	values := []int{0, 6}
+
+	err := ValidateMultiple(timeUnit, values)
+
+	assert.Equal(t, err, nil)
+}
+
+func TestDayOfWeekShouldReturnErrorIfValueIsLessThanLowerBound(t *testing.T) {
+	timeUnit := DayOfWeek()
+	value := -1
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid day of week value -1 outside bounds 0 and 6")
+}
+
+func TestDayOfWeekShouldReturnErrorIfValueIsGreaterThanUpperBound(t *testing.T) {
+	timeUnit := DayOfWeek()
+	value := 7
+
+	err := Validate(timeUnit, value)
+
+	assert.Equal(t, err.Error(), "invalid day of week value 7 outside bounds 0 and 6")
 }
 
 func allRangeValuesIncluding(min, max int) []int {
